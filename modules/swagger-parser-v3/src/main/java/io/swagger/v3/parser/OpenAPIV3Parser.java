3d0cc1cf355f6ac9e947704c97a77e4434e0c028
Fady Moussallam <fmoussallam@talend.com>
2019-01-25 17:03:48 +0100

    Properties order lost while converting from v2 to v3 #997

--- modules/swagger-parser-v3/src/main/java/io/swagger/v3/parser/OpenAPIV3Parser.java
+++ modules/swagger-parser-v3/src/main/java/io/swagger/v3/parser/OpenAPIV3Parser.java
@@ -200,7 +200,8 @@ public class OpenAPIV3Parser implements SwaggerParserExtension {
     protected List<SwaggerParserExtension> getExtensions() {
         List<SwaggerParserExtension> extensions = new ArrayList<>();
 
-        ServiceLoader<SwaggerParserExtension> loader = ServiceLoader.load(SwaggerParserExtension.class);
+        ServiceLoader<SwaggerParserExtension> loader = ServiceLoader.load(SwaggerParserExtension.class,
+                this.getClass().getClassLoader());
         Iterator<SwaggerParserExtension> itr = loader.iterator();
         while (itr.hasNext()) {
             extensions.add(itr.next());
